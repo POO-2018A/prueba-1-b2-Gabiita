0e0415651885fd3526c132d6e7f6632a79e46fd2
Gabiita <38547554+Gabiita@users.noreply.github.com>
2018-07-10 09:05:38 -0500

    Add files via upload

---
+++ TiendaFrutas/src/tiendafrutas/Frutas.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiendafrutas;
+
+/**
+ *
+ * @author ESFOT
+ */
+public class Frutas {
+    private String codigo;
+    private String nombre;
+    private String precio; 
+
+
+    public Frutas(String codigo, String nombre, String precio) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+     
+    }
+
+  
+    
+    
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+   
+    
+    
+}
